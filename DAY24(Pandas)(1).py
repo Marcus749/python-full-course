54f01741eec6df7b570a0418113b581667c7323c
gyan_prakash <gyan100485@gmail.com>
2026-03-12 20:33:25 +0530

    Uncomment code for data processing steps

--- DAY24(Pandas)(1).py
+++ DAY24(Pandas)(1).py
@@ -62,60 +62,10 @@ mumbai_emp.to_csv("Mumbai_emp.csv", index=False)
 
 
 
-# # Employees with Salary > 60000
-# high_salary = df[df["Salary"] > 60000]
-# print(high_salary)
-
-
-# print("\nEmployees from Mumbai:", mumbai_emp.shape[0])
-# print("Employees with Salary > 60000:", high_salary.shape[0])
-
-
-# # 6. SORT DATA
-
-# Sort by Salary (Descending)
-# df_sorted_salary = df.sort_values("Salary", ascending=False)
-# print(df_sorted_salary.head())
-
-# Sort by Joining Year
-# df_sorted_year = df.sort_values("Joining_Year")
-# print(df_sorted_year.head())
-
-
-# # 7. ADD NEW COLUMNS
-
-# # Salary Category
-# df["Salary_Category"] = df["Salary"].apply(
-#     lambda x: "High" if x >= 60000 else "Medium" if x >= 50000 else "Low"
-# )
-# print(df.head())
-
-# Experience (Years)
-# df["Experience"] = 2025 - df["Joining_Year"]
-# print(df.head())
-
-
-
-# # 8. GROUP BY OPERATIONS 
-
-# # Average Salary by Department
-# avg_salary_dept = df.groupby("Department")["Salary"].mean()
-# print(avg_salary_dept)
-
-# Total Salary by City
-# df["City"] = df["City"].str.strip().str.title()
-# total_salary_city = df.groupby("City")["Salary"].sum()
-# print(total_salary_city)
-
-# # Employee Count by Department
-# count_dept = df.groupby("Department")["Employee_ID"].count()
-# print(count_dept)
 
 
 
 
-# # # # 9. EXPORT CLEANED DATA - Sort by Salary (Descending)
-# df_sorted_salary = df.sort_values("Salary", ascending=False)
-# # print(df_sorted_salary)
-# df_sorted_salary.to_csv("employee_sorted_by_salary.csv", index=False)
+
+
 
