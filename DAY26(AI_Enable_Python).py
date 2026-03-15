30a1fa0255ac804cf322625a391859f5d243916c
gyan_prakash <gyan100485@gmail.com>
2026-03-15 19:48:39 +0530

    Add files via upload

---
+++ DAY26(AI_Enable_Python).py
@@ -0,0 +1,28 @@
+import pandas as pd
+from datetime import datetime
+
+# Load CSV
+df = pd.read_csv("D:\R ProgrammingL\Python-micro-course\python-full-course\day24_Employee_Data (1).csv")
+
+# 1. Clean names and cities (trim spaces, proper-case)
+# df['Employee_Name'] = df['Employee_Name'].astype(str).str.strip().str.title()
+# df['City'] = df['City'].astype(str).str.strip().str.title()
+
+# print(df.head())
+
+
+# # 2. Remove duplicates based on Employee_ID (or entire row)
+# df = df.drop_duplicates(subset=['Department'], keep='first')
+# print(df)
+
+# 3. Add Experience column (current_year - joining_year)
+# current_year = datetime.now().year
+# df['Experience'] = current_year - df['Joining_Year'].astype(int)
+
+# print(df)
+
+# # Display cleaned data
+# print(df.head())
+
+# # Save cleaned file (optional)
+# df.to_csv("day24_Employee_Data_Cleaned.csv", index=False)
